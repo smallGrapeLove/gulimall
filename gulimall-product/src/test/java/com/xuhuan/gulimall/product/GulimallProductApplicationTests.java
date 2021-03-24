2618653ce112f429932fd30bd85562b9454dff20
momo_xh0817@163.com <momo_xh0817@163.com>
2021-03-24 21:43:09 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/xuhuan/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuhuan.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
